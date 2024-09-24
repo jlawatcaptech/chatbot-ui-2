dd9e85d068571972c665e154628ca76be2f96102
Julian Schaefer <jschaefer@capitoltechsolutions.com>
2024-09-24 15:07:13 -0700

    added error message for Supabase connection

--- lib/supabase/browser-client.ts
+++ lib/supabase/browser-client.ts
@@ -1,6 +1,15 @@
 import { Database } from "@/supabase/types"
 import { createBrowserClient } from "@supabase/ssr"
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error(
+    "Your project's URL and Key are required to create a Supabase client! Check your Supabase project's API settings to find these values https://supabase.com/dashboard/project/_/settings/api"
+  )
+}
+
 export const supabase = createBrowserClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!

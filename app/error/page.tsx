8e4c73d8068c38556f7573851a48115f7dd23568
Julian Schaefer <jschaefer@capitoltechsolutions.com>
2024-09-25 11:46:20 -0700

    +updated middleware to check for authentification (WIP) + removed locale and restructured /app router

---
+++ app/error/page.tsx
@@ -0,0 +1,10 @@
+const AccessDenied = () => {
+  return (
+    <div className="flex h-screen items-center justify-center text-center text-red-500">
+      <h1 className="mb-12">Access Denied</h1>
+      <p>You do not have permission to access this page.</p>
+    </div>
+  )
+}
+
+export default AccessDenied
---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+const TestMiddlewarePage = () => {
+  return (
+    <div>
+      <h1>Test Middleware Page</h1>
+    </div>
+  )
+}
+
+export default TestMiddlewarePage

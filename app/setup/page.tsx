178baf7804a8136765c459bedaf20bcb57ca6f0f
Julian Schaefer <jschaefer@capitoltechsolutions.com>
2024-09-25 13:31:39 -0700

    + corrected route

--- app/setup/page.tsx
+++ app/setup/page.tsx
@@ -14,13 +14,13 @@ import { supabase } from "@/lib/supabase/browser-client"
 import { TablesUpdate } from "@/supabase/types"
 import { useRouter } from "next/navigation"
 import { useContext, useEffect, useState } from "react"
-import { APIStep } from "../../../components/setup/api-step"
-import { FinishStep } from "../../../components/setup/finish-step"
-import { ProfileStep } from "../../../components/setup/profile-step"
+import { APIStep } from "@/components/setup/api-step"
+import { FinishStep } from "@/components/setup/finish-step"
+import { ProfileStep } from "@/components/setup/profile-step"
 import {
   SETUP_STEP_COUNT,
   StepContainer
-} from "../../../components/setup/step-container"
+} from "@/components/setup/step-container"
 
 export default function SetupPage() {
   const {

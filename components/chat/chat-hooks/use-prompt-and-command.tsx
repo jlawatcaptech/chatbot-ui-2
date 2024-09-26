6536f251e662f09689de295377a379dc8fce8ff5
Julian Schaefer <jschaefer@capitoltechsolutions.com>
2024-09-26 11:03:17 -0700

    +Tokencount in prompt submit area

--- components/chat/chat-hooks/use-prompt-and-command.tsx
+++ components/chat/chat-hooks/use-prompt-and-command.tsx
@@ -188,6 +188,7 @@ export const usePromptAndCommand = () => {
     handleSelectUserFile,
     handleSelectUserCollection,
     handleSelectTool,
-    handleSelectAssistant
+    handleSelectAssistant,
+    tokenCount
   }
 }
